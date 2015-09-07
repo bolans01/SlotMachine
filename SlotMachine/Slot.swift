c243eb37a89408421a1bab5eb6d0dd8c008e0859
bolans01 <bolans01@gmail.com>
2015-09-06 21:44:01 -0400

    First commit

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Neel Bola on 7/2/15.
+//  Copyright (c) 2015 Neel Bola. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
